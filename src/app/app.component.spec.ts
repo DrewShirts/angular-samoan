c172d88d77b69558f1cd38de698fe3c2069b2c66
Drew Shirts <shirtsdre@churchofjesuschrist.org>
2021-06-15 18:32:27 -0600

    Updated page content and translations

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Access Denied');
+    expect(compiled.querySelector('h1').textContent).toContain('Localization Test for Samoan');
   });
 });
