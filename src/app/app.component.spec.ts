132a98d1932ccf1db4b39523a7dc67a0a90083c0
Drew Shirts <shirtsdre@churchofjesuschrist.org>
2021-06-15 18:04:36 -0600

    Updated tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-samoan app is running!');
+    expect(compiled.querySelector('h1').textContent).toContain('Access Denied');
   });
 });
