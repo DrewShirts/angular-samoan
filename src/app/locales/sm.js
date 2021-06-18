ea2ee2ec8727c171acc5d92b06993ab32c988398
Drew Shirts <shirtsdre@churchofjesuschrist.org>
2021-06-18 13:39:17 -0600

    Updates to the sm.js file

--- src/app/locales/sm.js
+++ src/app/locales/sm.js
@@ -18,52 +18,40 @@
   Object.defineProperty(exports, "__esModule", { value: true });
 // THIS CODE IS GENERATED - DO NOT MODIFY
 // See angular/tools/gulp-tasks/cldr/extract.js
-  var u = undefined;
-  function plural(n) {
-      var i = Math.floor(Math.abs(n)), v = n.toString().replace(/^[^.]*\.?/, '').length, f = parseInt(n.toString().replace(/^[^.]*\.?/, ''), 10) || 0;
-      if (v === 0 && (i === 1 || i === 2 || i === 3) ||
-          v === 0 && !(i % 10 === 4 || i % 10 === 6 || i % 10 === 9) ||
-          !(v === 0) && !(f % 10 === 4 || f % 10 === 6 || f % 10 === 9))
-          return 1;
-return 5;
+var u = undefined;
+function plural(n) {
+  return 5;
 }
 
 exports.default = [
 'sm',
-[['a', 'p'], ['AM', 'PM'], u],
-[['AM', 'PM'], u, u],
+[['AM', 'PM'], u, ['AM', 'PM']],
+[['AM', 'PM'], u, ['AM', 'PM']],
 [
-  ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-  ['Aso Sa', 'Aso Gafua', 'Aso Lua', 'Aso Lulu', 'Aso Tofi', 'Aso Faralie', 'Aso Tonaʻi'],
-  ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa']
+  ['S', 'G', 'L', 'L', 'T', 'F', 'T'],
+  ['Aso Sā', 'Aso Gafua', 'Aso Lua', 'Aso Lulu', 'Aso Tofi', 'Aso Faralie', 'Aso Toʻanaʻi'],
+  ['Sā', 'Ga', 'Lu', 'Lu', 'to', 'Fa', 'To']
 ],
 u,
 [
-  ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+  ['I', 'F', 'M', 'A', 'M', 'I', 'I', 'A', 'S', 'O', 'N', 'T'],
+  ['Ian', 'Fep', 'Mar', 'Apr', 'Me', 'Iun', 'Iul', 'Aug', 'Set', 'Oke', 'Nov', 'Tes'],
   [
     'Ianuari', 'Fepuari', 'Mati', 'Aperila', 'Me', 'Iuni', 'Iulai', 'Auguso', 'Setema',
     'Oketopa', 'Novema', 'Tesema'
   ]
 ],
-[
-  ['E', 'P', 'M', 'A', 'M', 'Hun', 'Hul', 'Ago', 'Set', 'Okt', 'Nob', 'Dis'],
-  ['Ene', 'Peb', 'Mar', 'Abr', 'May', 'Hun', 'Hul', 'Ago', 'Set', 'Okt', 'Nob', 'Dis'],
-  [
-    'Enero', 'Pebrero', 'Marso', 'Abril', 'Mayo', 'Hunyo', 'Hulyo', 'Agosto', 'Setyembre',
-    'Oktubre', 'Nobyembre', 'Disyembre'
-  ]
-],
-[['B', 'A'], ['BC', 'AD'], ['Before Christ', 'Anno Domini']],
+[['BC', 'AD'], u, ['I luma o Keriso', 'Anno Domini']],
 0,
 [6, 0],
-['M/d/yy', 'MMM d, y', 'MMMM d, y', 'EEEE, MMMM d, y'],
+['d/M/yy', 'd MMM y', 'd MMMM y', 'EEEE d MMMM y'],
 ['h:mm a', 'h:mm:ss a', 'h:mm:ss a z', 'h:mm:ss a zzzz'],
-['{1}, {0}', u, '{1} \'at\' {0}', u],
+['{1} {0}', '{1}, {0}', u, u],
 ['.', ',', ';', '%', '+', '-', 'E', '×', '‰', '∞', 'NaN', ':'],
 ['#,##0.###', '#,##0%', '¤#,##0.00', '#E0'],
-'USD',
-'$',
-'US Dollar',
+'Tālā',
+'WS$',
+'Samoan Tālā',
 {},
 'ltr',
 plural
